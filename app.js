0795fa62180e39373d6d69e42282ec46fb9a98ad
Lautaro Bernardo <lautibernardo@gmail.com>
2025-07-04 14:07:31 -0300

    main

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 var createError = require('http-errors');
-var express = require('express');
+const express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-/*Manejador 4*/
+//var usersRouter = require('./routes/users');
 
-var destacadosRouter =require('./routes/destacados') //destacados.js 
-
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,30 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-app.use('/destacados', destacadosRouter)
-
-/*ruta 1*/
-
-app.get('/prueba', function(req, res){
-  res.send('hola soy la pagina de prueba')
-})
-
-/*ruta 2*/
-
-app.get('/productos', function(req, res){
-  res.send('hola soy la pagina de PRODUCTOS')
-})
-
-/*ruta 3*/
-
-app.get('/contactanos', function(req, res){
-  res.send('hola soy la pagina de CONTACTANOS')
-})
-
-
-
+//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
